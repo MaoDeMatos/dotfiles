c945007f651e9bb5e34ad58ccc2bb36baa5f50b4
Mao De Matos <mao.dematos@gmail.com>
2025-07-23 21:33:48 +0200

    🎨 Minor cleanup on `zsh` config

--- private_dot_config/zsh/main.zsh
+++ private_dot_config/zsh/main.zsh
@@ -1,12 +1,9 @@
-# When installing nvim with pre-built archives
-export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
-
-# Golang
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export GOBIN=$GOPATH/bin
-export GO111MODULE=on
-export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+# # Golang
+# export GOROOT=/usr/local/go
+# export GOPATH=$HOME/go
+# export GOBIN=$GOPATH/bin
+# export GO111MODULE=on
+# export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 
 # Rust
 [ -s "$HOME/.cargo/env" ] && \. "$HOME/.cargo/env"
@@ -17,10 +14,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# Pyton
+# Python
 # uv & uvx completions
-eval "$(uv generate-shell-completion zsh)"
-eval "$(uvx --generate-shell-completion zsh)"
+if command -v uv >/dev/null 2>&1; then
+  eval "$(uv generate-shell-completion zsh)"
+  eval "$(uvx --generate-shell-completion zsh)"
+fi
 
 # Bun
 export BUN_INSTALL="/home/mao/.bun"
@@ -36,5 +35,10 @@ export PATH="$BUN_INSTALL/bin:$PATH"
 # Chezmoi completion
 source ~/.config/zsh/chezmoi-cmp.zsh
 
+# # Show system info
+# if [[ $- == *i* ]]; then
+#   clear && fastfetch
+# fi
+
 # custom aliases
-alias cdp='cd ~/projects'
+alias sf='fastfetch'
