6e5511ed0eed65657ab5ff309886a4a37e31083c
Mao De Matos <mao.dematos@gmail.com>
2025-07-25 18:20:02 +0200

    🔧 nvim: Automatically set current working dir when opening `nvim`
    This is used when opening a directory directly.
    Tools like file trees and telescope will search in the right dir
    automatically.
    eg: `nvim ~/.config`

--- private_dot_config/nvim/lua/autocmds.lua
+++ private_dot_config/nvim/lua/autocmds.lua
@@ -1,1 +1,14 @@
 require "nvchad.autocmds"
+
+-- Set working directory to the current file's directory on startup
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  group = vim.api.nvim_create_augroup("SetWorkingDir", { clear = true }),
+  callback = function()
+    -- Get the full path
+    local file_dir = vim.fn.expand "%:p:h"
+    if file_dir ~= "" then
+      -- Use the buffer-local 'lcd' equivalent
+      vim.cmd("lcd " .. file_dir)
+    end
+  end,
+})
