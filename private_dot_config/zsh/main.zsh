6086a53ef72dd449b0d28f038ff1243ac3ea4456
Mao De Matos <mao.dematos@gmail.com>
2025-08-11 18:01:51 +0200

    🎨 Cleaup & speed up `zsh`

--- private_dot_config/zsh/main.zsh
+++ private_dot_config/zsh/main.zsh
@@ -1,26 +1,3 @@
-# # Golang
-# export GOROOT=/usr/local/go
-# export GOPATH=$HOME/go
-# export GOBIN=$GOPATH/bin
-# export GO111MODULE=on
-# export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
-
-# Rust
-[ -s "$HOME/.cargo/env" ] && \. "$HOME/.cargo/env"
-[ -s "$HOME/.local/bin/env" ] && \. "$HOME/.local/bin/env"
-
-# Node version manager
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Python
-# uv & uvx completions
-if command -v uv >/dev/null 2>&1; then
-  eval "$(uv generate-shell-completion zsh)"
-  eval "$(uvx --generate-shell-completion zsh)"
-fi
-
 # Bun
 export BUN_INSTALL="/home/mao/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
@@ -32,13 +9,10 @@ export PATH="$BUN_INSTALL/bin:$PATH"
 #   export FZF_DEFAULT_OPTS='-m --reverse'
 # fi
 
-# Chezmoi completion
-source ~/.config/zsh/chezmoi-cmp.zsh
-
-# # Show system info
-# if [[ $- == *i* ]]; then
-#   clear && fastfetch
-# fi
+# Show system info
+if [[ $- == *i* ]]; then
+  fastfetch
+fi
 
 # custom aliases
 alias sf='fastfetch'
